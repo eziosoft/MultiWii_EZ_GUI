b42306a2bbcef01c858089212fff50b75eef73d6
eziosoft <eziosoft@gmail.com>
2012-11-17 03:26:58 +0100

    dashboard 2 scaling

--- src/com/ezio/multiwii/dashboard/Dashboard2View.java
+++ src/com/ezio/multiwii/dashboard/Dashboard2View.java
@@ -63,22 +63,19 @@ public class Dashboard2View extends View {
 	public int TXRSSI = 0;
 	public int RXRSSI = 0;
 
-	static int textSizeSmall = 20;
-	static int textSizeMedium = 40;
-	static int textSizeBig = 80;
+	private static float scale = 0.1f;
 
-	NumberFormat format = new DecimalFormat(
-			"0.############################################################"); // used
-																				// to
-																				// avoid
-																				// scientific
-																				// notation
+	static int textSizeSmall = (int) (20 * scale);
+	static int textSizeMedium = (int) (40 * scale);
+	static int textSizeBig = (int) (80 * scale);
 
-	public void Set(int satNum, float distanceToHome, float directionToHome,
-			float speed, float gpsAltitude, float altitude, float lat,
-			float lon, float pitch, float roll, float azimuth, float gforce,
-			String state, int vbat, int powerSum, int powerTrigger, int txRSSI,
-			int rxRSSI) {
+	NumberFormat format = new DecimalFormat("0.############################################################"); // used
+																												// to
+																												// avoid
+																												// scientific
+																												// notation
+
+	public void Set(int satNum, float distanceToHome, float directionToHome, float speed, float gpsAltitude, float altitude, float lat, float lon, float pitch, float roll, float azimuth, float gforce, String state, int vbat, int powerSum, int powerTrigger, int txRSSI, int rxRSSI) {
 		SatNum = satNum;
 		DistanceToHome = distanceToHome;
 		DirectionToHome = directionToHome;
@@ -111,6 +108,11 @@ public class Dashboard2View extends View {
 		getWindowVisibleDisplayFrame(dim);
 		ww = dim.width();
 		hh = dim.height();
+
+		scale = hh / 480;
+		textSizeSmall = (int) (20 * scale);
+		textSizeMedium = (int) (40 * scale);
+		textSizeBig = (int) (80 * scale);
 
 		p = new Paint();
 		p.setColor(Color.GREEN);
@@ -197,8 +199,7 @@ public class Dashboard2View extends View {
 		a += 5;
 		p.setTextSize(textSizeMedium);
 		c.drawRect(new Rect(0, a, 150, a + textSizeSmall), p);
-		c.drawRect(new Rect(0, a, (int) map(TXRSSI, 0, 110, 0, 150), a
-				+ textSizeSmall), p4);
+		c.drawRect(new Rect(0, a, (int) map(TXRSSI, 0, 110, 0, 150), a + textSizeSmall), p4);
 
 		a += textSizeSmall * 2;
 		p.setTextSize(textSizeSmall);
@@ -207,8 +208,7 @@ public class Dashboard2View extends View {
 		a += 5;
 		p.setTextSize(textSizeMedium);
 		c.drawRect(new Rect(0, a, 150, a + textSizeSmall), p);
-		c.drawRect(new Rect(0, a, (int) map(RXRSSI, 0, 110, 0, 150), a
-				+ textSizeSmall), p4);
+		c.drawRect(new Rect(0, a, (int) map(RXRSSI, 0, 110, 0, 150), a + textSizeSmall), p4);
 
 		a = hh - textSizeMedium;
 		p.setTextSize(textSizeSmall);
@@ -216,196 +216,170 @@ public class Dashboard2View extends View {
 
 		a = hh;
 		p.setTextSize(textSizeMedium);
-		c.drawText(
-				format.format(Lat / Math.pow(10, 7)) + " x "
-						+ format.format(Lon / Math.pow(10, 7)), 0, a, p);
+		c.drawText(format.format(Lat / Math.pow(10, 7)) + " x " + format.format(Lon / Math.pow(10, 7)), 0, a, p);
 
 		// //////////////////////////////
 		a = hh - textSizeMedium;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.Azimuth),
-				ww - p.measureText(context.getString(R.string.Azimuth)), a, p);
+		c.drawText(context.getString(R.string.Azimuth), ww - p.measureText(context.getString(R.string.Azimuth)), a, p);
 
 		a = hh;
 		p.setTextSize(textSizeMedium);
-		c.drawText(String.valueOf(Azimuth),
-				ww - p.measureText(String.valueOf(Azimuth)), a, p);
+		c.drawText(String.valueOf(Azimuth), ww - p.measureText(String.valueOf(Azimuth)), a, p);
 
 		a -= textSizeMedium + textSizeSmall;
 		p.setTextSize(textSizeMedium);
-		c.drawText(Integer.toString((int) Pitch),
-				ww - p.measureText(Integer.toString((int) Pitch)), a, p);
+		c.drawText(Integer.toString((int) Pitch), ww - p.measureText(Integer.toString((int) Pitch)), a, p);
 
 		a -= textSizeMedium;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.Pitch),
-				ww - p.measureText(context.getString(R.string.Pitch)), a, p);
+		c.drawText(context.getString(R.string.Pitch), ww - p.measureText(context.getString(R.string.Pitch)), a, p);
 
 		a -= textSizeSmall;
 		p.setTextSize(textSizeMedium);
-		c.drawText(Integer.toString((int) Roll),
-				ww - p.measureText(Integer.toString((int) Roll)), a, p);
+		c.drawText(Integer.toString((int) Roll), ww - p.measureText(Integer.toString((int) Roll)), a, p);
 
 		a -= textSizeMedium;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.Roll),
-				ww - p.measureText(context.getString(R.string.Roll)), a, p);
+		c.drawText(context.getString(R.string.Roll), ww - p.measureText(context.getString(R.string.Roll)), a, p);
 
 		a -= textSizeSmall;
 		p.setTextSize(textSizeMedium);
-		c.drawText(String.format("%.2f", Altitude),
-				ww - p.measureText(String.format("%.2f", Altitude)), a, p);
+		c.drawText(String.format("%.2f", Altitude), ww - p.measureText(String.format("%.2f", Altitude)), a, p);
 
 		a -= textSizeMedium;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.GPS_altitude),
-				ww - p.measureText(context.getString(R.string.GPS_altitude)),
-				a, p);
+		c.drawText(context.getString(R.string.GPS_altitude), ww - p.measureText(context.getString(R.string.GPS_altitude)), a, p);
 
 		a -= textSizeSmall;
 		p.setTextSize(textSizeMedium);
-		c.drawText(String.format("%.2f", Gforce),
-				ww - p.measureText(String.format("%.2f", Gforce)), a, p);
+		c.drawText(String.format("%.2f", Gforce), ww - p.measureText(String.format("%.2f", Gforce)), a, p);
 
 		a -= textSizeMedium;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.gforce),
-				ww - p.measureText(context.getString(R.string.gforce)), a, p);
+		c.drawText(context.getString(R.string.gforce), ww - p.measureText(context.getString(R.string.gforce)), a, p);
 
 		a = 0;
 		a += textSizeSmall;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.state),
-				ww - p.measureText(context.getString(R.string.state)), a, p);
+		c.drawText(context.getString(R.string.state), ww - p.measureText(context.getString(R.string.state)), a, p);
 
 		a += textSizeMedium;
 		p.setTextSize(textSizeMedium);
-		c.drawText(String.valueOf(State),
-				ww - p.measureText(String.valueOf(State)), a, p);
+		c.drawText(String.valueOf(State), ww - p.measureText(String.valueOf(State)), a, p);
 
 		a += textSizeSmall;
 		p.setTextSize(textSizeSmall);
-		c.drawText(context.getString(R.string.Battery),
-				ww - p.measureText(context.getString(R.string.Battery)), a, p);
+		c.drawText(context.getString(R.string.Battery), ww - p.measureText(context.getString(R.string.Battery)), a, p);
 
 		a += textSizeMedium;
 		p.setTextSize(textSizeMedium);
-		c.drawText(String.valueOf(VBat),
-				ww - p.measureText(String.valueOf(VBat)), a, p);
+		c.drawText(String.valueOf(VBat), ww - p.measureText(String.valueOf(VBat)), a, p);
 
 		a += textSizeSmall;
 		p.setTextSize(textSizeSmall);
-		c.drawText(
-				context.getString(R.string.PowerSumPowerTrigger),
-				ww
-						- p.measureText(context
-								.getString(R.string.PowerSumPowerTrigger)), a,
-				p);
+		c.drawText(context.getString(R.string.PowerSumPowerTrigger), ww - p.measureText(context.getString(R.string.PowerSumPowerTrigger)), a, p);
 
 		a += textSizeMedium;
 		p.setTextSize(textSizeMedium);
-		c.drawText(
-				String.valueOf(PowerSum) + "/" + String.valueOf(PowerTrigger),
-				ww
-						- p.measureText(String.valueOf(PowerSum) + "/"
-								+ String.valueOf(PowerTrigger)), a, p);
+		c.drawText(String.valueOf(PowerSum) + "/" + String.valueOf(PowerTrigger), ww - p.measureText(String.valueOf(PowerSum) + "/" + String.valueOf(PowerTrigger)), a, p);
 
 		float x1, y1, x2, y2;
-		x1 = (float) (200 * Math.sin((-Roll - 90) * Math.PI / 180)) + ww / 2;
-		y1 = (float) ((float) ((200 * Math.cos((-Roll - 90) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x1 = (float) (200 * scale * Math.sin((-Roll - 90) * Math.PI / 180)) + ww / 2;
+		y1 = (float) ((float) ((200 * scale * Math.cos((-Roll - 90) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
-		x2 = (float) (200 * Math.sin((-Roll - 270) * Math.PI / 180)) + ww / 2;
-		y2 = (float) ((float) ((200 * Math.cos((-Roll - 270) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x2 = (float) (200 * scale * Math.sin((-Roll - 270) * Math.PI / 180)) + ww / 2;
+		y2 = (float) ((float) ((200 * scale * Math.cos((-Roll - 270) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
 		c.drawLine(x1, y1, x2, y2, p1);
 
-		x1 = (float) (200 * Math.sin((-Roll - 45) * Math.PI / 180)) + ww / 2;
-		y1 = (float) ((float) ((200 * Math.cos((-Roll - 45) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x1 = (float) (200 * scale * Math.sin((-Roll - 45) * Math.PI / 180)) + ww / 2;
+		y1 = (float) ((float) ((200 * scale * Math.cos((-Roll - 45) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
-		x2 = (float) (200 * Math.sin((-Roll - 315) * Math.PI / 180)) + ww / 2;
-		y2 = (float) ((float) ((200 * Math.cos((-Roll - 315) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x2 = (float) (200 * scale * Math.sin((-Roll - 315) * Math.PI / 180)) + ww / 2;
+		y2 = (float) ((float) ((200 * scale * Math.cos((-Roll - 315) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
 		c.drawLine(x1, y1, x2, y2, p3);
 
-		x1 = (float) (200 * Math.sin((-Roll - 135) * Math.PI / 180)) + ww / 2;
-		y1 = (float) ((float) ((200 * Math.cos((-Roll - 135) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x1 = (float) (200 * scale * Math.sin((-Roll - 135) * Math.PI / 180)) + ww / 2;
+		y1 = (float) ((float) ((200 * scale * Math.cos((-Roll - 135) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
-		x2 = (float) (200 * Math.sin((-Roll - 225) * Math.PI / 180)) + ww / 2;
-		y2 = (float) ((float) ((200 * Math.cos((-Roll - 225) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
+		x2 = (float) (200 * scale * Math.sin((-Roll - 225) * Math.PI / 180)) + ww / 2;
+		y2 = (float) ((float) ((200 * scale * Math.cos((-Roll - 225) * Math.PI / 180)) + hh / 2) - (Pitch) / 35 * 200 / 2);
 
 		c.drawLine(x1, y1, x2, y2, p3);
 
 		// ////
 
-		x1 = (float) (200 * Math.sin((-Roll + 180) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 180) * Math.PI / 180)) + hh / 2;
+		x1 = (float) (200 * scale * Math.sin((-Roll + 180) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 180) * Math.PI / 180)) + hh / 2;
 
-		x2 = (float) (180 * Math.sin((-Roll + 180) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (180 * Math.cos((-Roll + 180) * Math.PI / 180)) + hh / 2;
-
-		c.drawLine(x1, y1, x2, y2, p1);
-
-		x1 = (float) (200 * Math.sin((-Roll + 190) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 190) * Math.PI / 180)) + hh / 2;
-
-		x2 = (float) (190 * Math.sin((-Roll + 190) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 190) * Math.PI / 180)) + hh / 2;
-
-		c.drawLine(x1, y1, x2, y2, p3);
-
-		x1 = (float) (200 * Math.sin((-Roll + 200) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 200) * Math.PI / 180)) + hh / 2;
-
-		x2 = (float) (190 * Math.sin((-Roll + 200) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 200) * Math.PI / 180)) + hh / 2;
-
-		c.drawLine(x1, y1, x2, y2, p3);
-
-		x1 = (float) (200 * Math.sin((-Roll + 210) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 210) * Math.PI / 180)) + hh / 2;
-
-		x2 = (float) (190 * Math.sin((-Roll + 210) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 210) * Math.PI / 180)) + hh / 2;
-
-		c.drawLine(x1, y1, x2, y2, p3);
-
-		x1 = (float) (200 * Math.sin((-Roll + 225) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 225) * Math.PI / 180)) + hh / 2;
-
-		x2 = (float) (190 * Math.sin((-Roll + 225) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 225) * Math.PI / 180)) + hh / 2;
+		x2 = (float) (180 * scale * Math.sin((-Roll + 180) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (180 * scale * Math.cos((-Roll + 180) * Math.PI / 180)) + hh / 2;
 
 		c.drawLine(x1, y1, x2, y2, p1);
 
-		x1 = (float) (200 * Math.sin((-Roll + 170) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 170) * Math.PI / 180)) + hh / 2;
+		x1 = (float) (200 * scale * Math.sin((-Roll + 190) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 190) * Math.PI / 180)) + hh / 2;
 
-		x2 = (float) (190 * Math.sin((-Roll + 170) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 170) * Math.PI / 180)) + hh / 2;
-
-		c.drawLine(x1, y1, x2, y2, p3);
-
-		x1 = (float) (200 * Math.sin((-Roll + 160) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 160) * Math.PI / 180)) + hh / 2;
-
-		x2 = (float) (190 * Math.sin((-Roll + 160) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 160) * Math.PI / 180)) + hh / 2;
+		x2 = (float) (190 * scale * Math.sin((-Roll + 190) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 190) * Math.PI / 180)) + hh / 2;
 
 		c.drawLine(x1, y1, x2, y2, p3);
 
-		x1 = (float) (200 * Math.sin((-Roll + 150) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 150) * Math.PI / 180)) + hh / 2;
+		x1 = (float) (200 * scale * Math.sin((-Roll + 200) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 200) * Math.PI / 180)) + hh / 2;
 
-		x2 = (float) (190 * Math.sin((-Roll + 150) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 150) * Math.PI / 180)) + hh / 2;
+		x2 = (float) (190 * scale * Math.sin((-Roll + 200) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 200) * Math.PI / 180)) + hh / 2;
 
 		c.drawLine(x1, y1, x2, y2, p3);
 
-		x1 = (float) (200 * Math.sin((-Roll + 135) * Math.PI / 180)) + ww / 2;
-		y1 = (float) (200 * Math.cos((-Roll + 135) * Math.PI / 180)) + hh / 2;
+		x1 = (float) (200 * scale * Math.sin((-Roll + 210) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 210) * Math.PI / 180)) + hh / 2;
 
-		x2 = (float) (190 * Math.sin((-Roll + 135) * Math.PI / 180)) + ww / 2;
-		y2 = (float) (190 * Math.cos((-Roll + 135) * Math.PI / 180)) + hh / 2;
+		x2 = (float) (190 * scale * Math.sin((-Roll + 210) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 210) * Math.PI / 180)) + hh / 2;
+
+		c.drawLine(x1, y1, x2, y2, p3);
+
+		x1 = (float) (200 * scale * Math.sin((-Roll + 225) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 225) * Math.PI / 180)) + hh / 2;
+
+		x2 = (float) (190 * scale * Math.sin((-Roll + 225) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 225) * Math.PI / 180)) + hh / 2;
+
+		c.drawLine(x1, y1, x2, y2, p1);
+
+		x1 = (float) (200 * scale * Math.sin((-Roll + 170) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 170) * Math.PI / 180)) + hh / 2;
+
+		x2 = (float) (190 * scale * Math.sin((-Roll + 170) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 170) * Math.PI / 180)) + hh / 2;
+
+		c.drawLine(x1, y1, x2, y2, p3);
+
+		x1 = (float) (200 * scale * Math.sin((-Roll + 160) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 160) * Math.PI / 180)) + hh / 2;
+
+		x2 = (float) (190 * scale * Math.sin((-Roll + 160) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 160) * Math.PI / 180)) + hh / 2;
+
+		c.drawLine(x1, y1, x2, y2, p3);
+
+		x1 = (float) (200 * scale * Math.sin((-Roll + 150) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 150) * Math.PI / 180)) + hh / 2;
+
+		x2 = (float) (190 * scale * Math.sin((-Roll + 150) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 150) * Math.PI / 180)) + hh / 2;
+
+		c.drawLine(x1, y1, x2, y2, p3);
+
+		x1 = (float) (200 * scale * Math.sin((-Roll + 135) * Math.PI / 180)) + ww / 2;
+		y1 = (float) (200 * scale * Math.cos((-Roll + 135) * Math.PI / 180)) + hh / 2;
+
+		x2 = (float) (190 * scale * Math.sin((-Roll + 135) * Math.PI / 180)) + ww / 2;
+		y2 = (float) (190 * scale * Math.cos((-Roll + 135) * Math.PI / 180)) + hh / 2;
 
 		c.drawLine(x1, y1, x2, y2, p1);
 
@@ -414,12 +388,11 @@ public class Dashboard2View extends View {
 		c.drawLine(ww / 2 - 10, hh / 2, ww / 2 - 30, hh / 2, p1);
 		c.drawLine(ww / 2 + 10, hh / 2, ww / 2 + 30, hh / 2, p1);
 
-		RectF r = new RectF(ww / 2 - 200, hh / 2 - 200, ww / 2 + 200,
-				hh / 2 + 200);
+		RectF r = new RectF(ww / 2 - 200 * scale, hh / 2 - 200 * scale, ww / 2 + 200 * scale, hh / 2 + 200 * scale);
 		// c.drawRect(r, p);
 		c.drawArc(r, Roll - 45, -90, false, p1);
 		// c.drawArc(oval, startAngle, sweepAngle, useCenter, paint)
-		c.drawLine(ww / 2, hh / 2 - 200, ww / 2, hh / 2 - 230, p1);
+		c.drawLine(ww / 2, hh / 2 - 200 * scale, ww / 2, hh / 2 - 230 * scale, p1);
 
 	}
 
