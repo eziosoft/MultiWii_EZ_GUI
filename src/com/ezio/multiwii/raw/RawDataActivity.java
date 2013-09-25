304d7353063bb9ca3637f51da85d39aab1c0ef09
eziosoft <eziosoft@gmail.com>
2013-09-26 01:34:35 +0200

    bug fix

--- src/com/ezio/multiwii/raw/RawDataActivity.java
+++ src/com/ezio/multiwii/raw/RawDataActivity.java
@@ -275,12 +275,7 @@ public class RawDataActivity extends Activity {
 	}
 
 	public void SendOnClick(View v) {
-	
-		app.mw.SendRequestMSP_SET_RAW_GPS((byte)1, (byte)5, 488300660, 24414160, 23, 5);
-		
 		ShareIt();
-		
-	
 	}
 
 	public void EnableDebugOnClick(View v) {
@@ -291,10 +286,9 @@ public class RawDataActivity extends Activity {
 			app.Say("Debug version");
 			app.mw.GPS_longitude = 24414160;
 			app.mw.GPS_latitude = 488300660;
-			app.mw.Waypoints[0].Lat=488300660;
-			app.mw.Waypoints[0].Lon=24414160;
-			
-			
+			app.mw.Waypoints[0].Lat = 488300660;
+			app.mw.Waypoints[0].Lon = 24414160;
+
 		}
 
 	}
